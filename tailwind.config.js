9d267f90e0ef29cde561aca671a6c5278fb38ac8
Victor <krudoner@gmail.com>
2026-01-27 13:01:56 +0100

    guardado de imagenes y configurcion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*{html,js}", "./src/**/*.{js,ts,jsx,tsx}"],
+    content: ["./*{html,js}", "./src/**/*.{html,js,ts,jsx,tsx}"],
     theme: {
         fontFamily: {
             'headerText': ["Playfair Display", "sans-serif"],
